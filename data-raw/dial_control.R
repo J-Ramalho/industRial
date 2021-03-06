b4985fd63561c4f521dc2a9878766d8772f17b6a
J-Ramalho <ramalho.joao@protonmail.com>
2021-03-06 16:23:15 +0100

    test with 2nd dataset

--- data-raw/dial_control.R
+++ data-raw/dial_control.R
@@ -1,6 +1,8 @@
 ## code to prepare `dial_control` dataset goes here
 
 dial_control <- read.csv("data-raw/dial_control.csv")
+pet_delivery <- read.csv("data-raw/pet_delivery.csv")
 
 usethis::use_data(dial_control, overwrite = TRUE)
+usethis::use_data(pet_delivery, overwrite = TRUE)
 

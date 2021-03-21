ec3b828463b7e0165bdaaf4f6456b13ba17cbfb5
J-Ramalho <ramalho.joao@protonmail.com>
2021-03-21 14:19:53 +0100

    data split into separate use this files

--- data-raw/dial_control.R
+++ data-raw/dial_control.R
@@ -1,8 +1,7 @@
 ## code to prepare `dial_control` dataset goes here
 
 dial_control <- read.csv("data-raw/dial_control.csv")
-pet_delivery <- read.csv("data-raw/pet_delivery.csv")
 
 usethis::use_data(dial_control, overwrite = TRUE)
-usethis::use_data(pet_delivery, overwrite = TRUE)
+
 

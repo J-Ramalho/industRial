aaae11dffc4462da56ec390d381ad21597e0d553
J-Ramalho <ramalho.joao@protonmail.com>
2021-04-17 22:34:39 +0200

    renamed data raw with underscore again

--- data_raw/perfume.R
+++ data_raw/perfume.R
@@ -1,5 +1,5 @@
 ## code to prepare `perfume` dataset goes here
 
-perfume <- readxl::read_xlsx("data-raw/perfume.xlsx")
+perfume <- readxl::read_xlsx("data_raw/perfume.xlsx")
 
 usethis::use_data(perfume, overwrite = TRUE)

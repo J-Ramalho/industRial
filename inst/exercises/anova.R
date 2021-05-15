283f9e4f291023f9d48e702ef0a9a939409f8778
J-Ramalho <ramalho.joao@protonmail.com>
2021-05-15 13:11:17 +0200

    completed anova learnr

--- inst/exercises/anova.R
+++ inst/exercises/anova.R
@@ -2,37 +2,41 @@ library(tidyverse)
 library(industRial)
 library(stats)
 library(broom)
-library(ggridges)
 filter <- dplyr::filter
 select <- dplyr::select
 
 input <- list()
-input$mean1 <- 10
-input$mean2 <- 10.1
-input$mean3 <- 10.2
-input$sd1 <- 4
-input$sd2 <- 4
-input$sd3 <- 4
-input$n1 <- 10000
-input$n2 <- 10000
-input$n3 <- 10000
+input$mean1 <- 550000
+input$mean2 <- 550000
+input$mean3 <- 550000
+input$mean4 <- 550000
+input$sd1 <- 10
+input$sd2 <- 10
+input$sd3 <- 10
+input$sd4 <- 10
+input$n <- 100
 
 measurements <- tibble(
-  group1 = rnorm(mean = input$mean1, sd = input$sd1, n = input$n1),
-  group2 = rnorm(mean = input$mean2, sd = input$sd2, n = input$n2),
-  group3 = rnorm(mean = input$mean3, sd = input$sd3, n = input$n3)) %>% 
+  group1 = rnorm(mean = input$mean1, sd = input$sd1, n = input$n),
+  group2 = rnorm(mean = input$mean2, sd = input$sd2, n = input$n),
+  group3 = rnorm(mean = input$mean3, sd = input$sd3, n = input$n),
+  group4 = rnorm(mean = input$mean3, sd = input$sd3, n = input$n)) %>% 
   pivot_longer(
     cols = everything(), 
     names_to = "group",
-    values_to = "value")
+    values_to = "value") %>%
+  group_by(group) %>%
+  mutate(group_mean = mean(value))
 
 analysis <- aov(value ~ group, measurements)
 # group_pvalue <- analysis %>% tidy() %>% filter(term == "group") %>% pull(p.value)
 analysis_out <- tidy(analysis)
 analysis_out
 
-ggplot() +
-  geom_boxplot(data = measurements, aes(x = group, y = value)) +
+ggplot(measurements) +
+  geom_point(aes(x = group, y = group_mean), color = "red", size = 4) +
+  geom_boxplot(aes(x = group, y = value, fill= group)) +
+  scale_fill_viridis_d(option = "A", begin = 0.5) +
   labs(
     title = "Measurement Boxplot",
     subtile = "",
@@ -40,17 +44,5 @@ ggplot() +
     y = "Value"
   ) +
   theme_industRial()
-# 
-# ggplot(data = measurements, aes(y = value)) +
-#   geom_ridgeline(stat = "identity") +
-#   # facet_wrap(vars(group))
-#   labs(
-#     title = "Measurement Boxplot",
-#     subtile = "",
-#     x = "",
-#     y = "Value"
-#   ) +
-#   theme_industRial()
-
 
 
--- inst/exercises/aov.R
+++ inst/exercises/aov.R
@@ -5,19 +5,20 @@ library(stats)
 library(broom)
 filter <- dplyr::filter
 select <- dplyr::select
-# ready for push into epfl for prototype review
 
 ui <- fluidPage(
     titlePanel("The anova sandbox"),
     sidebarLayout(
         sidebarPanel(width = 4,
-            sliderInput(inputId = "mean1", "Group 1 target mean:", min = -5, max = 5, value = 0),
-            sliderInput(inputId = "mean2", "Group 2 target mean:", min = -5, max = 5, value = 0),
-            sliderInput(inputId = "mean3", "Group 3 targat mean:", min = -5, max = 5, value = 0),
-            sliderInput(inputId = "sd1", "Group 1 sd:", min = 1, max = 20, value = 10),
-            sliderInput(inputId = "sd2", "Group 2 sd:", min = 1, max = 20, value = 10),
-            sliderInput(inputId = "sd3", "Group 3 sd:", min = 1, max = 20, value = 10),
-            sliderInput(inputId = "n", "Group sample size n:", min = 100, max = 100, value = 50),
+            sliderInput(inputId = "mean1", "Group 1 target mean:", min = 500000, max = 700000, value = 540000),
+            sliderInput(inputId = "mean2", "Group 2 target mean:", min = 500000, max = 700000, value = 575000),
+            sliderInput(inputId = "mean3", "Group 3 target mean:", min = 500000, max = 700000, value = 625000),
+            sliderInput(inputId = "mean4", "Group 4 target mean:", min = 500000, max = 700000, value = 700000),
+            sliderInput(inputId = "sd1", "Group 1 sd:", min = 10000, max = 50000, value = 50000, step = 1000),
+            sliderInput(inputId = "sd2", "Group 2 sd:", min = 10000, max = 50000, value = 30000, step = 1000),
+            sliderInput(inputId = "sd3", "Group 3 sd:", min = 10000, max = 50000, value = 40000, step = 1000),
+            sliderInput(inputId = "sd4", "Group 4 sd:", min = 10000, max = 50000, value = 15000, step = 1000),
+            sliderInput(inputId = "n", "Group sample size n:", min = 100, max = 10000, value = 100),
         ),
         mainPanel(width = 8,
            plotOutput("boxplot"),
@@ -32,7 +33,8 @@ server <- function(input, output) {
         tibble(
             group1 = rnorm(mean = input$mean1, sd = input$sd1, n = input$n),
             group2 = rnorm(mean = input$mean2, sd = input$sd2, n = input$n),
-            group3 = rnorm(mean = input$mean3, sd = input$sd3, n = input$n)) %>% 
+            group3 = rnorm(mean = input$mean3, sd = input$sd3, n = input$n),
+            group4 = rnorm(mean = input$mean4, sd = input$sd4, n = input$n)) %>% 
         pivot_longer(
             cols = everything(), 
             names_to = "group",
@@ -44,17 +46,22 @@ server <- function(input, output) {
     output$boxplot <- renderPlot({
 
         ggplot(measurements()) +
-            geom_point(aes(x = group, y = group_mean), color = "red", size = 4) +
-            geom_jitter(aes(x = group, y = value), width = 0.1) +
+            # geom_point(aes(x = group, y = group_mean), color = "red", size = 4) +
+            geom_boxplot(aes(x = group, y = value, fill = group)) +
             labs(
                 title = "Measurements plot",
                 subtile = "",
                 x = "",
                 y = "Value"
             ) +
-            geom_hline(yintercept = 0, color = "blue") +
-            scale_y_continuous(n.breaks = 20) +
-            coord_cartesian(ylim = c(-30, 30)) +
+            geom_hline(yintercept = 540000, color = "red") +
+            scale_fill_viridis_d(option = "D", begin = 0.5) +
+            scale_y_continuous(n.breaks = 10) +
+            # coord_cartesian(ylim = c(500000, 600000)) +
+            labs(title = "e-bike frame hardening process",
+                 subtitle = "Raw data plot",
+                 x = "Furnace Temperature [°C]",
+                 y = "Cycles to failure [n]") +
             theme_industRial()
     })
     
